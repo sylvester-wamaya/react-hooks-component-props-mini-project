c7b525aee2a03df86ca312131a49a62ba641e7a1
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-07-23 20:28:52 +0300

    Assignment completed

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import react from "react";
+function About({image = "https://via.placeholder.com/215", about}){
+return (<aside>
+    <img src={image} alt="blog logo"/>
+    <p>{about}</p>
+</aside>
+)
+}
+export default About;
---
+++ src/components/Article.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Article({title,preview,date="January 1, 1970"}){
+    return (
+        <article>
+        <h3> {title}</h3>
+        <small>{date}</small>
+        <p>{preview}</p>
+        </article>
+    )}
+export default Article;
